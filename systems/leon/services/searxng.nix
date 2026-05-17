16a502f7de92c13d422b9642165b8de88bd30ee2
Loplolo <accounts@lopl.dev>
2026-05-17 19:50:12 +0200

    host pris: fix searxng timouts

--- systems/leon/services/searxng.nix
+++ systems/leon/services/searxng.nix
@@ -8,12 +8,17 @@
     enable = true;
 
     environmentFile = config.sops.secrets.searxng-secret.path;
-
     settings = {
       server = {
         port = 8082;
-        bind_address = "127.0.0.1";
+        bind_address = "0.0.0.0";
         secret_key = "@SEARXNG_SECRET_KEY@";
+        image_proxy = true;
+      };
+
+      outgoing = {
+        request_timeout = 5.0;
+        max_request_timeout = 10.0;
       };
 
       ui = {
