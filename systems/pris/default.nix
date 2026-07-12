2e11b50d40952871e5c4659fcf7c6f7b94c43c13
Loplolo <accounts@lopl.dev>
2026-07-12 12:00:51 +0200

    changed env vars from strings to integers

--- systems/pris/default.nix
+++ systems/pris/default.nix
@@ -258,9 +258,9 @@
   environment.variables = {
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    NIXOS_OZONE_WL = "1";
-    __GL_GSYNC_ALLOWED = "1";
-    __GL_VRR_ALLOWED = "1";
+    NIXOS_OZONE_WL = 1;
+    __GL_GSYNC_ALLOWED = 1;
+    __GL_VRR_ALLOWED = 1;
   };
 
   # xserver
