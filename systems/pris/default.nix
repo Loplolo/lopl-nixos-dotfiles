5272f89df9cb72e1a7b2e8a0a2c40110c07ba6bf
Loplolo <accounts@lopl.dev>
2026-07-12 06:28:17 +0200

    add modprobe config to fix screen sharing freeze issues

--- systems/pris/default.nix
+++ systems/pris/default.nix
@@ -319,7 +319,7 @@
     v4l2loopback
   ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    options v4l2loopback devices=1 video_nr=9 card_label="OBS Virtual Camera" exclusive_caps=1 max_buffers=8
   '';
 
   boot.loader.systemd-boot.enable = true;
